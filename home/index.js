199a4713f173ef5fe2ca56f0cf80d266955b2111
HonoredTarget <37421204+HonoredTarget@users.noreply.github.com>
2020-03-12 00:18:19 -0400

    Still pls no look

--- home/index.js
+++ home/index.js
@@ -9,42 +9,34 @@ $(document).ready(function () {
     );
     $("#2").hover(function () {
         // over
-        Enter1();
         Enter2();
 
     }, function () {
         // out
-        Exit1();
         Exit2();
     }
     );
 });
 
 function Exit1() {
-    $("#1").css("background-color", "rgb(165, 165, 165)");
-    $("#1").html("<h2>Tier 1</h2>");
+    $("#1").css("background-color", "rgba(128, 128, 128, 0.377)");
+    $("#1").css("color", "white");
+    $("#1").html("<h2>Super Charger</h2>");
 }
 
 function Enter1() {
     $("#1").css("background-color", "white");
-    $("#1").html("<h3>Car has autonomous abilities only on highways where roads do not have sharp turns. Parking autonomous ability not available in Omnix S tier one upgrade.</h3>");
+    $("#1").css("color", "black");
+    $("#1").html("<p style='text-align: left'><ul><li>Charger costs $800 (minus tax).</li><li>Can charge all three Omnix models in 67 minutes.</li><li>Comes with a seperate expandable solar panel to allow for emergency charging while on the road.</li><li>Can be set up at any home or place of work, as long as two free outlets are available.</li><li>Once you purchase a super charger, a network of chargers all across the world will become accessible to you.</li></ul></p>");
 }
 function Exit2() {
-    $("#2").css("background-color", "rgb(165, 165, 165)");
-    $("#2").html("<h2>Tier 2</h2>");
+    $("#2").css("background-color", "rgba(128, 128, 128, 0.377)");
+    $("#2").css("color", "white");
+    $("#2").html("<h2>Solar Panel</h2>");
 }
 
 function Enter2() {
     $("#2").css("background-color", "white");
-    $("#2").html("<h3>Parking autonomous ability is incorporated. Also, pre existing solar panels come with the Omnix A in order to provide an extra source of power to the Omnix A vehicle.</h3>");
+    $("#2").css("color", "black");
+    $("#2").html("<p style='text-align: left'><ul> <li>Solar panels cost $400 each (minus tax).</li><li>The solar panel can be mounted on any Omnix car. All Omnix cars come with pre existing mounts just for the solar panels.</li><li>Up to 3 solar panels can be placed on any car.</li><li>Each solar panel provides 9 extra hours of driving time.</li><li>Solar panels come with a lifetime warranty that is separate from the car itself.</li></ul></p>");
 }
-function Exit3() {
-    $("#3").css("background-color", "rgb(165, 165, 165)");
-    $("#3").html("<h2>Tier 3</h2>");
-}
-
-function Enter3() {
-    $("#3").css("background-color", "white");
-    $("#3").html("<h3>Full autonomous abilities available in the Tier Three upgrade. Also, a second (smaller) electric motor is put in to increase the top speed to 330 mph.</h3>");
-}
-
